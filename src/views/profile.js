c08786fb20708cae83aafd85d6fa0baf73a918e5
dcoa <dcoa@live.com>
2020-08-24 22:06:22 -0500

    Eliminar publicación

--- src/views/profile.js
+++ src/views/profile.js
@@ -2,13 +2,29 @@ import { currentUser } from '../lib/firebaseAuth.js';
 
 export default () => {
   const profile = document.createElement('section');
-  profile.setAttribute('id', 'timelineBody');
-  profile.innerHTML = `<p>Aqui va un comentario</p>`;
+  profile.setAttribute('id', 'profileBody');
+  profile.innerHTML = `<div class="comment">
+            <p>Aqui va un comentario</p>
+            <div id="deleteIcon"></div>
+            <div id="confirm">
+            <h2>¿Estás seguro que quieres eliminar la publicación?</h2>
+            <button type="submit" class="btn" id="deleteBtn">ELIMINAR</button>
+            </div>
+            </div>`;
   let user = currentUser();
-  console.log(user.photoURL);
-  profile.querySelector('p').addEventListener('click', () => {
-    console.log('esto esta muy loco se descuadro');
+
+  profile.querySelector('#deleteBtn').addEventListener('submit', ()=>{
+    e.preventDefault();
   });
 
+  window.addEventListener('click', (e)=>{
+  
+    if(e.target == profile.querySelector('#deleteIcon')){
+      profile.querySelector('#confirm').style.display = 'flex';
+    }else{
+      profile.querySelector('#confirm').style.display = 'none';
+    }
+
+  })
   return profile;
 };
